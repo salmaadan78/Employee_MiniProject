532198d962c409eb371ffae1fdf40ffbf85116be
Salma Adan <salmaadan@Salmas-MacBook-Pro.local>
2023-06-07 10:27:04 +0100

    new commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,9 @@
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
+
         EmployeeCsvReader reader = new EmployeeCsvReader();
-//        var result = reader.readFileLines("src/main/resources/Employees_short.csv");
-//        System.out.println(result);
 
     }
 }
+
