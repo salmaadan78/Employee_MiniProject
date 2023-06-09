65a6d19a16fe498dbad1c266c143157e6e096892
Salma Adan <salmaadan@Salmas-MacBook-Pro.local>
2023-06-09 21:07:34 +0100

    project

---
+++ src/main/java/EmployeeConversion.java
@@ -0,0 +1,29 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class EmployeeConversion {
+
+    public String employeeObjectToJSON(Object object) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(object);
+    }
+    public String employeeObjectToXML(Object object) throws JsonProcessingException {
+        XmlMapper xmlMapper = new XmlMapper();
+        return xmlMapper.writeValueAsString(object);
+    }
+
+    public Employee xmlToEmployeeObject (String value) throws JsonProcessingException {
+        XmlMapper xmlMapper = new XmlMapper();
+        return xmlMapper.readValue(value, Employee.class);
+    }
+
+    public Employee jsonToEmployeeObject (String value) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(value, Employee.class);
+    }
+
+
+
+
+}
