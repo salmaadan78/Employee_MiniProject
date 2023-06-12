fdd97fa5918e8a8ffd7aa59f825ec843f66c3565
tessthott <tessthott@gmail.com>
2023-06-12 10:15:16 +0100

    version2.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,11 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, JsonProcessingException {
 
-        EmployeeCsvReader reader = new EmployeeCsvReader();
-        ArrayList<Employee> empList = reader.readEmployee("src/main/resources/Employees_short.csv");
-        System.out.println(empList);
-        EmployeeConversion empConversion = new EmployeeConversion();
-            System.out.println(empConversion.employeeObjectToJSON(empList));
+           EmployeeCsvReader reader = new EmployeeCsvReader();
+           ArrayList<Employee> empList = reader.readEmployee("src/main/resources/Employees_short.csv");
+           System.out.println(empList);
+           EmployeeConversion empConversion = new EmployeeConversion();
+           System.out.println(empConversion.employeeObjectToJSON(empList));
 
     }
 }
